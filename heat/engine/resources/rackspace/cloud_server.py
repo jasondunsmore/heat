b5db7588ff50c05feb560f2914a948f86fa38b6d
Jason Dunsmore <jasondunsmore@gmail.com>
2013-06-11 09:43:17 -0500

    Refactoring

--- heat/engine/resources/rackspace/cloud_server.py
+++ heat/engine/resources/rackspace/cloud_server.py
@@ -16,10 +16,11 @@ logger = logging.getLogger(__name__)
 
 
 class CloudServer(resource.Resource):
-    tags_schema = {'Key': {'Type': 'String',
-                           'Required': True},
-                   'Value': {'Type': 'String',
-                             'Required': True}}
+    tags_schema = {
+        'Key': {'Type': 'String', 'Required': True},
+        'Value': {'Type': 'String', 'Required': True}
+    }
+
     properties_schema = {
         'UserData': {'Type': 'String'},
         'KeyName': {'Type': 'String'},
@@ -33,7 +34,7 @@ class CloudServer(resource.Resource):
         'SubnetId': {'Type': 'String'},
         'NetworkInterfaces': {'Type': 'List'},
         'InstanceName': {'Type': 'String', 'Required': True},
-        'InstanceType': {'Type': 'String', 'Required': True},
+        'Flavor': {'Type': 'String', 'Required': True},
         'ImageName': {'Type': 'String', 'Required': True},
         'Tags': {'Type': 'List',
                  'Schema': {'Type': 'Map',
@@ -42,11 +43,11 @@ class CloudServer(resource.Resource):
 
     rackspace_images = {
         "F17": "76856c8b-e56e-4301-b454-c8cd1be22cfb",
-        "U10.04": "d4c7b93d-9f18-45dc-aa7c-3e3b126e3792",
         "U12.04": "e4dbdba7-b2a4-4ee5-8e8f-4595b6d694ce"
     }
 
-    f17_script = """#!/bin/bash -e
+    image_scripts = {
+        "F17": """#!/bin/bash -e
 
 # Install cloud-init and heat-cfntools
 yum install -y cloud-init python-boto
@@ -65,11 +66,13 @@ bash /var/lib/cloud/data/cfn-userdata
 # Clean up
 rm -f /root/.ssh/authorized_keys
 """
+    }
 
     def __init__(self, name, json_snippet, stack):
         super(CloudServer, self).__init__(name, json_snippet, stack)
         self.ipaddress = None
         self.mime_string = None
+        self.cs = pyrax.connect_to_cloudservers()
 
     def _create_container_name(self, name=None):
         return name or '%s-%s-%s' % (self.stack.name,
@@ -119,9 +122,16 @@ rm -f /root/.ssh/authorized_keys
 
         Rackspace Cloud Servers does not have the metadata service
         running, so we have to transfer the user-data file to the
-        server.
+        server and then trigger cloud-init.
 
         """
+
+        def create_temp_file(data):
+            temp_file = tempfile.NamedTemporaryFile()
+            temp_file.write(data)
+            temp_file.seek(0)
+            return temp_file
+
         # Retrieve auth info from file (temporary solution)
         pyrax.set_setting("identity_type", "rackspace")
         pyrax.set_credential_file("/opt/stack/heat/heat/engine/resources/rackspace/rs-pyrax-creds.txt")
@@ -130,48 +140,38 @@ rm -f /root/.ssh/authorized_keys
         name = self.properties['InstanceName']
         image_name = self.properties['ImageName']
         image_id = self.rackspace_images[image_name]
-        # TODO(jason): Map instance types to RS flavors
-        #flavor = self.properties['InstanceType']
-        flavor = "2"
+        script = self.image_scripts[image_name]
+        flavor = self.properties['Flavor']
+        raw_userdata = self.properties['UserData'] or ''
+        userdata = self._build_userdata(raw_userdata)
 
         # Generate one-time-use SSH public/private keypair (public key
         # will be put on server using personalities)
         rsa = RSA.generate(1024)
         private_key = rsa.exportKey()
-        private_key_file = tempfile.NamedTemporaryFile()
-        private_key_file.write(private_key)
-        private_key_file.seek(0)
         public_key = rsa.publickey().exportKey('OpenSSH')
         files={"/root/.ssh/authorized_keys": public_key}
 
-        # Create userdata file
-        raw_userdata = self.properties['UserData'] or ''
-        userdata = self._build_userdata(raw_userdata)
-        userdata_file = tempfile.NamedTemporaryFile()
-        userdata_file.write(userdata)
-        userdata_file.seek(0)
-
         # Create server
-        cs_ord = pyrax.connect_to_cloudservers(region="ORD")
-        server = cs_ord.servers.create(name, image_id, flavor, files=files)
+        server = self.cs.servers.create(name, image_id, flavor, files=files)
         complete = pyrax.utils.wait_until(server, "status",
                                           ["ACTIVE", "ERROR"], attempts=0)
 
         # Get public IP
-        for public_ip in complete.addresses['public']:
-            if public_ip['version'] == 4:
-                ip = public_ip['addr']
-        if not ip:
+        for ip in complete.addresses['public']:
+            if ip['version'] == 4:
+                public_ip = public_ip['addr']
+        if not public_ip:
             raise exception.Error('Could not determine public IP of server')
 
-        # Create a temp file for config script
-        script_file = tempfile.NamedTemporaryFile()
-        script_file.write(self.f17_script)
-        script_file.seek(0)
+        # Create temp files for SFTP
+        userdata_file = create_temp_file(userdata)
+        script_file = create_temp_file(script)
+        private_key_file = create_temp_file(private_key)
 
         # Transfer files to server via SFTP
         pkey = paramiko.RSAKey.from_private_key_file(private_key_file.name)
-        transport = paramiko.Transport((ip, 22))
+        transport = paramiko.Transport((public_ip, 22))
         transport.connect(hostkey=None, username="root", pkey=pkey)
         sftp = paramiko.SFTPClient.from_transport(transport)
         sftp.put(userdata_file.name, "/tmp/userdata")
@@ -180,8 +180,7 @@ rm -f /root/.ssh/authorized_keys
         # Connect via SSH and run script
         ssh = paramiko.SSHClient()
         ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        private_key_file.seek(0)
-        ssh.connect(ip,
+        ssh.connect(public_ip,
                     username="root",
                     key_filename=private_key_file.name)
         stdin, stdout, stderr = ssh.exec_command("bash /root/heat-script")
