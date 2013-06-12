6f7b80ed26acc5eea614d181f79df3a1e0852511
Jason Dunsmore <jasondunsmore@gmail.com>
2013-06-12 13:34:53 -0500

    Remove code duplication from instance.py
    Change-Id: I9bac9bee327d395df96462be7dc46df3758f6cf8

--- heat/engine/resources/rackspace/cloud_server.py
+++ heat/engine/resources/rackspace/cloud_server.py
@@ -11,23 +11,15 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
-import os
-import json
-import pkgutil
 import tempfile
 import paramiko
 import pyrax
-from urlparse import urlparse
 
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
 from Crypto.PublicKey import RSA
-
-from oslo.config import cfg
 
 from heat.engine.resources import instance
 from heat.openstack.common import log as logging
-from heat.common import short_id, exception
+from heat.common import exception
 
 logger = logging.getLogger(__name__)
 
@@ -88,79 +80,6 @@ rm -f /root/.ssh/authorized_keys
         "F18": fedora_script
     }
 
-    def __init__(self, name, json_snippet, stack):
-        super(CloudServer, self).__init__(name, json_snippet, stack)
-        self.ipaddress = None
-        self.mime_string = None
-
-    def _create_container_name(self, name=None):
-        return name or '%s-%s-%s' % (self.stack.name,
-                                     self.name,
-                                     short_id.generate_id())
-
-    def _build_userdata(self, userdata):
-        if not self.mime_string:
-            # Build mime multipart data blob for cloudinit userdata
-
-            def make_subpart(content, filename, subtype=None):
-                if subtype is None:
-                    subtype = os.path.splitext(filename)[0]
-                msg = MIMEText(content, _subtype=subtype)
-                msg.add_header('Content-Disposition', 'attachment',
-                               filename=filename)
-                return msg
-
-            def read_cloudinit_file(fn):
-                data = pkgutil.get_data('heat', 'cloudinit/%s' % fn)
-                data = data.replace('@INSTANCE_USER@',
-                                    cfg.CONF.instance_user)
-                return data
-
-            attachments = [(read_cloudinit_file('config'), 'cloud-config'),
-                           (read_cloudinit_file('boothook.sh'), 'boothook.sh',
-                            'cloud-boothook'),
-                           (read_cloudinit_file('part-handler.py'),
-                            'part-handler.py'),
-                           (userdata, 'cfn-userdata', 'x-cfninitdata'),
-                           (read_cloudinit_file('loguserdata.py'),
-                            'loguserdata.py', 'x-shellscript')]
-
-            if 'Metadata' in self.t:
-                attachments.append((json.dumps(self.metadata),
-                                    'cfn-init-data', 'x-cfninitdata'))
-
-            attachments.append((cfg.CONF.heat_watch_server_url,
-                                'cfn-watch-server', 'x-cfninitdata'))
-
-            attachments.append((cfg.CONF.heat_metadata_server_url,
-                                'cfn-metadata-server', 'x-cfninitdata'))
-
-            # Create a boto config which the cfntools on the host use to know
-            # where the cfn and cw API's are to be accessed
-            cfn_url = urlparse(cfg.CONF.heat_metadata_server_url)
-            cw_url = urlparse(cfg.CONF.heat_watch_server_url)
-            is_secure = cfg.CONF.instance_connection_is_secure
-            vcerts = cfg.CONF.instance_connection_https_validate_certificates
-            boto_cfg = "\n".join(["[Boto]",
-                                  "debug = 0",
-                                  "is_secure = %s" % is_secure,
-                                  "https_validate_certificates = %s" % vcerts,
-                                  "cfn_region_name = heat",
-                                  "cfn_region_endpoint = %s" %
-                                  cfn_url.hostname,
-                                  "cloudwatch_region_name = heat",
-                                  "cloudwatch_region_endpoint = %s" %
-                                  cw_url.hostname])
-            attachments.append((boto_cfg,
-                                'cfn-boto-cfg', 'x-cfninitdata'))
-
-            subparts = [make_subpart(*args) for args in attachments]
-            mime_blob = MIMEMultipart(_subparts=subparts)
-
-            self.mime_string = mime_blob.as_string()
-
-        return self.mime_string
-
     def handle_create(self):
         """Create a Rackspace Cloud Servers container.
 
@@ -219,7 +138,7 @@ rm -f /root/.ssh/authorized_keys
         transport.connect(hostkey=None, username="root", pkey=pkey)
         sftp = paramiko.SFTPClient.from_transport(transport)
         remote_userdata = sftp.open("/tmp/userdata", 'w')
-        remote_userdata.write(userdata)
+        remote_userdata.write(self.userdata)
         remote_userdata.close()
         remote_script = sftp.open("/root/heat-script", 'w')
         remote_script.write(script)
@@ -258,10 +177,6 @@ rm -f /root/.ssh/authorized_keys
             server.delete()
 
         self.resource_id = None
-
-    def handle_update(self, json_snippet, tmpl_diff, prop_diff):
-        if 'Metadata' in tmpl_diff:
-            self.metadata = tmpl_diff.get('Metadata', {})
 
 
 def resource_mapping():
