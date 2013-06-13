4537e5d1ce1bcb9bf64ad805cafb93fb67f41197
Jason Dunsmore <jasondunsmore@gmail.com>
2013-06-13 09:57:59 -0500

    Add Cloud Server test file.
    Change-Id: Iec0d894c68d8d7d61bbc730163f98fbb00024db8

---
+++ heat/tests/test_rackspace_cloud_server.py
@@ -0,0 +1,96 @@
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+import copy
+
+import mox
+
+from heat.tests.v1_1 import fakes
+from heat.common import template_format
+from heat.engine import parser
+from heat.engine import resource
+from heat.engine import scheduler
+from heat.openstack.common import uuidutils
+from heat.tests.common import HeatTestCase
+from heat.tests import utils
+from heat.tests.utils import setup_dummy_db
+from heat.engine.resources.rackspace import cloud_server
+
+
+wp_template = '''
+{
+  "AWSTemplateFormatVersion" : "2010-09-09",
+  "Description" : "WordPress",
+  "Parameters" : {
+    "Flavor" : {
+      "Description" : "Rackspace Cloud Server flavor",
+      "Type" : "String",
+      "Default" : "2",
+      "AllowedValues" : [ "2", "3", "4", "5", "6", "7", "8" ],
+      "ConstraintDescription" : "must be a valid Rackspace Cloud Server flavor."
+    },
+  },
+  "Resources" : {
+    "WebServer": {
+      "Type": "Rackspace::Cloud::Server",
+      "Properties": {
+        "ImageName"      : "F17",
+        "InstanceName"   : "Heat test",
+        "Flavor"         : "2",
+        "UserData"       : "wordpress"
+      }
+    }
+  }
+}
+'''
+
+
+class RackspaceCloudServerTest(HeatTestCase):
+    def setUp(self):
+        super(RackspaceCloudServerTest, self).setUp()
+        self.fc = fakes.FakeClient()
+        setup_dummy_db()
+
+    def _setup_test_instance(self, return_server, name):
+        stack_name = '%s_stack' % name
+        t = template_format.parse(wp_template)
+        template = parser.Template(t)
+        params = parser.Parameters(stack_name, template, {'KeyName': 'test'})
+        stack = parser.Stack(None, stack_name, template, params,
+                             stack_id=uuidutils.generate_uuid())
+
+        t['Resources']['WebServer']['Properties']['ImageName'] = 'F17'
+        t['Resources']['WebServer']['Properties']['InstanceType'] = \
+            '256 MB Server'
+        server = cloud_server.CloudServer('%s_name' % name,
+                                          t['Resources']['WebServer'], stack)
+
+        self.m.StubOutWithMock(instance, 'nova')
+        instance.nova().MultipleTimes().AndReturn(self.fc)
+
+        instance.t = instance.stack.resolve_runtime_data(instance.t)
+
+        # need to resolve the template functions
+        server_userdata = instance._build_userdata(
+            instance.t['Properties']['UserData'])
+        self.m.StubOutWithMock(self.fc.servers, 'create')
+        self.fc.servers.create(
+            image=1, flavor=1, key_name='test',
+            name=utils.PhysName(stack_name, instance.name),
+            security_groups=None,
+            userdata=server_userdata, scheduler_hints=None,
+            meta=None, nics=None, availability_zone=None).AndReturn(
+                return_server)
+
+        return server
+
+    
