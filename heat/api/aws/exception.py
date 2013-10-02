e48577f0c8eecea48a57dedc4d696224c9db54a1
Jason Dunsmore <jasondunsmore@gmail.com>
2013-10-02 15:22:12 -0500

    Implement stack-locking for multi-engine support
    Implements a distributed stack lock using the database to avoid race
    conditions when multiple engines are deployed.
    
    Blueprint multiple-engines
    
    Change-Id: If3c47dafcc5bc1b2188b7737205291bbab8bc231

--- heat/api/aws/exception.py
+++ heat/api/aws/exception.py
@@ -252,6 +252,16 @@ class HeatAPINotImplementedError(HeatAPIException):
     err_type = "Server"
 
 
+class HeatActionInProgressError(HeatAPIException):
+    '''
+    Cannot perform action on stack in its current state
+    '''
+    code = 400
+    title = 'InvalidAction'
+    explanation = ("Cannot perform action on stack while other actions are " +
+                   "in progress")
+
+
 def map_remote_error(ex):
         """
         Map rpc_common.RemoteError exceptions returned by the engine
@@ -276,6 +286,7 @@ def map_remote_error(ex):
         )
         denied_errors = ('Forbidden', 'NotAuthorized')
         already_exists_errors = ('StackExists')
+        invalid_action_errors = ('ActionInProgress',)
 
         ex_type = ex.__class__.__name__
 
@@ -288,6 +299,8 @@ def map_remote_error(ex):
             return HeatAccessDeniedError(detail=str(ex))
         elif ex_type in already_exists_errors:
             return AlreadyExistsError(detail=str(ex))
+        elif ex_type in invalid_action_errors:
+            return HeatActionInProgressError(detail=str(ex))
         else:
             # Map everything else to internal server error for now
             return HeatInternalFailureError(detail=str(ex))
---
+++ heat/engine/stack_lock.py
@@ -0,0 +1,94 @@
+
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from oslo.config import cfg
+
+cfg.CONF.import_opt('engine_life_check_timeout', 'heat.common.config')
+
+from heat.common import exception
+from heat.db import api as db_api
+
+from heat.openstack.common import log as logging
+from heat.openstack.common import uuidutils
+from heat.openstack.common.rpc import proxy
+from heat.openstack.common.rpc.common import Timeout
+
+logger = logging.getLogger(__name__)
+engine_id = uuidutils.generate_uuid()
+
+
+class StackLock(object):
+    def __init__(self, context, stack):
+        self.context = context
+        self.stack = stack
+        self.listener = None
+
+    def _engine_alive(self, engine_id):
+        topic = engine_id
+        rpc = proxy.RpcProxy(topic, "1.0")
+        msg = rpc.make_msg("listening")
+        try:
+            return rpc.call(self.context, msg, topic=topic,
+                            timeout=cfg.CONF.engine_life_check_timeout)
+        except Timeout:
+            return False
+
+    def acquire(self):
+        """Acquire a lock on the stack."""
+        lock_type, lock_engine_id = db_api.stack_lock_create(self.stack.id,
+                                                             engine_id)
+        if lock_type == "new":
+            logger.debug("Engine %s acquired lock on stack %s (%s)"
+                         % (engine_id, self.stack.id, self.stack.action))
+            return
+
+        if lock_engine_id == engine_id or self._engine_alive(lock_engine_id):
+            logger.debug("Stack lock is owned by engine %s" % lock_engine_id)
+            raise exception.ActionInProgress(stack_name=self.stack.name,
+                                             action=self.stack.action)
+        else:
+            logger.info("Stale lock detected.  Engine %s will attempt "
+                        "to steal the lock" % engine_id)
+
+            rows_affected = db_api.stack_lock_steal(self.stack.id,
+                                                    lock_engine_id, engine_id)
+
+            # Either the old_engine_id didn't match (lost race) or
+            # the lock was *just* released.
+            if rows_affected == 0:
+                raise exception.ActionInProgress(
+                    stack_name=self.stack.name, action=self.stack.action)
+
+            logger.info("Engine %s successfully stole the lock"
+                        % engine_id)
+
+    def release(self):
+        """Release a stack lock."""
+        # Only the engine that owns the lock will be releasing it.
+        # This ensures the lock being released is
+        rows_affected = db_api.stack_lock_release(self.stack.id, engine_id)
+        if rows_affected == 0:
+            logger.debug("Lock was already released on stack %s (%s)"
+                         % (self.stack.id, self.stack.action))
+        else:
+            logger.debug("Engine %s released lock on stack %s (%s)"
+                         % (engine_id, self.stack.id, self.stack.action))
+
+    def _gt_callback_release(self, gt, *args, **kwargs):
+        """Callback function that will be passed to GreenThread.link()."""
+        # If gt.wait() isn't called here and a lock exists, then the
+        # pending _gt_callback_release() from the previous acquire()
+        # will be executed immediately upon the next call to
+        # acquire().  This leads to a pre-mature release of the lock.
+        gt.wait()
+        self.release()
