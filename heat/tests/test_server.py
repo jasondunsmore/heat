5ff71f2548ac0d54a6c13b32a6b144737418e53f
Jason Dunsmore <jasondunsmore@gmail.com>
2014-02-06 12:57:09 -0600

    Handle API 500 response in OS::Nova::Server
    novaclient raises a ClientException when a 500 response code is returned
    from the Nova API.  Previously, this would cause the stack action to
    fail.  Log a warning instead.
    
    Closes-Bug: #1276517
    Change-Id: I8f00d476c8d7166aca74fdcd3e0f2621c00d9802

--- heat/tests/test_server.py
+++ heat/tests/test_server.py
@@ -20,6 +20,7 @@ from heat.engine import environment
 from heat.tests.v1_1 import fakes
 from heat.common import exception
 from heat.common import template_format
+from heat.engine import clients
 from heat.engine import parser
 from heat.engine import resource
 from heat.engine import scheduler
@@ -143,6 +144,57 @@ class ServersTest(HeatTestCase):
         self.assertEqual('sample-server2', server.FnGetAtt('instance_name'))
         self.assertEqual('192.0.2.0', server.FnGetAtt('accessIPv4'))
         self.assertEqual('::babe:4317:0A83', server.FnGetAtt('accessIPv6'))
+        self.m.VerifyAll()
+
+    def test_server_create_500_error(self):
+        return_server = self.fc.servers.list()[0]
+        server = self._setup_test_server(return_server, 'test_create_500')
+        server.resource_id = 1234
+
+        check_iterations = [0]
+
+        # Bind fake get method which Server.check_create_complete will call
+        def activate_status(server):
+            check_iterations[0] += 1
+            if check_iterations[0] == 1:
+                server.status = 'BUILD'
+            elif check_iterations[0] == 2:
+                msg = ("ClientException: The server has either erred or is "
+                       "incapable of performing the requested operation.")
+                raise clients.novaclient.exceptions.ClientException(500, msg)
+            elif check_iterations[0] > 2:
+                server.status = 'ACTIVE'
+        return_server.get = activate_status.__get__(return_server)
+
+        self.m.ReplayAll()
+        scheduler.TaskRunner(server.create)()
+        self.assertEqual('CREATE', server.action)
+        self.assertEqual('COMPLETE', server.status)
+        self.m.VerifyAll()
+
+    def test_server_suspend_500_error(self):
+        return_server = self.fc.servers.list()[1]
+        server = self._create_test_server(return_server, 'test_suspend_500')
+        server.resource_id = 1234
+
+        # Override the get_servers_1234 handler
+        d1 = {'server': self.fc.client.get_servers_detail()[1]['servers'][0]}
+        d2 = copy.deepcopy(d1)
+        d1['server']['status'] = 'ACTIVE'
+        d2['server']['status'] = 'SUSPENDED'
+        self.m.StubOutWithMock(self.fc.client, 'get_servers_1234')
+        get = self.fc.client.get_servers_1234
+        get().AndReturn((200, d1))
+        get().AndReturn((200, d1))
+        msg = ("ClientException: The server has either erred or is "
+               "incapable of performing the requested operation.")
+        get().AndRaise(clients.novaclient.exceptions.ClientException(500, msg))
+        get().AndReturn((200, d2))
+        self.m.ReplayAll()
+
+        scheduler.TaskRunner(server.suspend)()
+        self.assertEqual('SUSPEND', server.action)
+        self.assertEqual('COMPLETE', server.status)
         self.m.VerifyAll()
 
     def test_server_create_metadata(self):
