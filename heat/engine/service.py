7edcf2156fba9b382283c6e8a69c4096cc40704d
Jason Dunsmore <jasondunsmore@gmail.com>
2014-12-29 14:37:33 -0600

    Add ability to hide stacks based on tag
    Add a "hidden_stack_tags" option that contains a list of tag names.
    Stacks that have one or more of these tags will be hidden.
    
    Add an option to show hidden stacks in the stack listing.
    
    blueprint stack-tags
    
    Change-Id: I45a5ac6d73a9a61629a56f88270e3a97fafb378a

--- heat/engine/service.py
+++ heat/engine/service.py
@@ -314,7 +314,8 @@ class EngineService(service.Service):
         admin_context = context.get_admin_context()
         stacks = stack_object.Stack.get_all(
             admin_context,
-            tenant_safe=False)
+            tenant_safe=False,
+            show_hidden=True)
         for s in stacks:
             self.stack_watch.start_watch_task(s.id, admin_context)
 
@@ -463,7 +464,7 @@ class EngineService(service.Service):
     @context.request_context
     def list_stacks(self, cnxt, limit=None, marker=None, sort_keys=None,
                     sort_dir=None, filters=None, tenant_safe=True,
-                    show_deleted=False, show_nested=False):
+                    show_deleted=False, show_nested=False, show_hidden=False):
         """
         The list_stacks method returns attributes of all stacks.  It supports
         pagination (``limit`` and ``marker``), sorting (``sort_keys`` and
@@ -478,17 +479,19 @@ class EngineService(service.Service):
         :param tenant_safe: if true, scope the request by the current tenant
         :param show_deleted: if true, show soft-deleted stacks
         :param show_nested: if true, show nested stacks
+        :param show_hidden: if true, show hidden stacks
         :returns: a list of formatted stacks
         """
         stacks = parser.Stack.load_all(cnxt, limit, marker, sort_keys,
                                        sort_dir, filters, tenant_safe,
                                        show_deleted, resolve_data=False,
-                                       show_nested=show_nested)
+                                       show_nested=show_nested,
+                                       show_hidden=show_hidden)
         return [api.format_stack(stack) for stack in stacks]
 
     @context.request_context
     def count_stacks(self, cnxt, filters=None, tenant_safe=True,
-                     show_deleted=False, show_nested=False):
+                     show_deleted=False, show_nested=False, show_hidden=False):
         """
         Return the number of stacks that match the given filters
         :param cnxt: RPC context.
@@ -496,6 +499,7 @@ class EngineService(service.Service):
         :param tenant_safe: if true, scope the request by the current tenant
         :param show_deleted: if true, count will include the deleted stacks
         :param show_nested: if true, count will include nested stacks
+        :param show_hidden: if true, show hidden stacks
         :returns: a integer representing the number of matched stacks
         """
         return stack_object.Stack.count_all(
@@ -503,7 +507,8 @@ class EngineService(service.Service):
             filters=filters,
             tenant_safe=tenant_safe,
             show_deleted=show_deleted,
-            show_nested=show_nested)
+            show_nested=show_nested,
+            show_hidden=show_hidden)
 
     def _validate_deferred_auth_context(self, cnxt, stack):
         if cfg.CONF.deferred_auth_method != 'password':
