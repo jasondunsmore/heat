2dafaf5943d14bfc6b4e061d112a7c45acc95992
Randall Burt <randall.burt@rackspace.com>
2015-04-14 17:24:31 -0500

    Ignore ConnectionError on container delete
    If the target Docker API is unavailable on delete, assume
    we have lost connection to the server and can no longer
    reach the container.
    
    Change-Id: I2a57f7aa4e0209ddaa590353b8d07851e3cd31c8
    Closes-Bug: #1444173

--- contrib/heat_docker/heat_docker/resources/docker_container.py
+++ contrib/heat_docker/heat_docker/resources/docker_container.py
@@ -15,9 +15,10 @@
 #    under the License.
 
 import distutils
+import six
 
 from oslo_log import log as logging
-import six
+from requests import exceptions
 
 from heat.common import exception
 from heat.common.i18n import _
@@ -489,6 +490,10 @@ class DockerContainer(resource.Resource):
         except docker.errors.APIError as ex:
             if ex.response.status_code != 404:
                 raise
+        except exceptions.ConnectionError:
+            # we've lost connection to the target api so assume we can no
+            # longer reach the container
+            return
         return self.resource_id
 
     def check_delete_complete(self, container_id):
