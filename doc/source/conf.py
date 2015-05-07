8cc368e82191aac726bcf3f1134db13fac09ba4b
Randall Burt <randall.burt@rackspace.com>
2015-05-07 16:04:36 -0500

    Re-fix doc generation
    Change-Id: If8d221d48b82e07421e1fa534b66f0a35e201378
    
    Conflicts:
    	doc/source/ext/resources.py
    
    Conflicts:
    	doc/source/conf.py
    
    Hide Monasca resources
    
    Change-Id: I2ed87291f464e9ea36e4fbbfa7a78cd71f307211
    
    Remove requirements from upstream Rackspace contrib
    
    These requirements will be covered in the rax-ext repo
    
    Change-Id: If020fca19a39c0f0c03b5b7766cb05a38d6f3109
    
    Conflicts:
    	contrib/rackspace/requirements.txt
    
    Conflicts:
    	doc/source/conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -327,4 +327,4 @@ texinfo_documents = [
 # How to display URL addresses: 'footnote', 'no', or 'inline'.
 # texinfo_show_urls = 'footnote'
 
-text_sectionchars = '*=-~"+`*=-~"+`'
+text_sectionchars = '=-^"~+`<>^#_$@'
